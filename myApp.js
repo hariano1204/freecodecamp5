8b32171763f364fff6d50f719ad69ada31586068
Tomás Saavedra <saavedratomas294@gmail.com>
2025-10-04 23:58:11 -0300

    servidor

--- myApp.js
+++ myApp.js
@@ -3,9 +3,12 @@ console.log("hello world");
 
 let express = require('express');
 let app = express();
+const path = require("path");
+
 app.get("/", function(req, res) {
-  res.send("Hello Express");
+  res.sendFile(path.join(__dirname, "/views/index.html"));
 });
+
 
 
 
