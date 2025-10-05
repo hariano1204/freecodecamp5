35cc5316131f9bb98e0affc35111e509985caae0
Tomás Saavedra <saavedratomas294@gmail.com>
2025-10-05 02:14:01 -0300

    body

--- myApp.js
+++ myApp.js
@@ -11,6 +11,9 @@ app.use(function (req, res, next) {
 app.use("/public", express.static(__dirname + "/public"));
 
 const path = require("path");
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/views/index.html"));
@@ -49,7 +52,12 @@ app.get("/name", function (req, res) {
   res.json({ name: `${firstName} ${lastName}` });
 });
 
-
+// Nueva ruta POST
+app.post("/name", function (req, res) {
+  const firstName = req.body.first;
+  const lastName = req.body.last;
+  res.json({ name: `${firstName} ${lastName}` });
+});
 
 
 
