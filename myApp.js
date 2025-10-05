627523ed252aad254f8d6528b49e749e89dc67a6
Tomás Saavedra <saavedratomas294@gmail.com>
2025-10-05 01:06:05 -0300

    middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,11 @@ console.log("hello world");
 
 let express = require('express');
 let app = express();
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 const path = require("path");
